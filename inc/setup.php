9892c9027c46c0549e96f4bf376be2e81e584cfe
Maarten Menten <mentenmaarten@gmail.com>
2019-08-07 00:43:02 +0200

    Updated content_width global

--- inc/setup.php
+++ inc/setup.php
@@ -95,7 +95,7 @@ function my_theme_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'my_theme_content_width', 1920 );
+	$GLOBALS['content_width'] = apply_filters( 'my_theme_content_width', 1140 );
 }
 
 add_action( 'after_setup_theme', 'my_theme_content_width', 0 );
