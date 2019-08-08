b32e02f9f1045a1e0c1cc5f1a50ecb94a5321e3d
Maarten Menten <mentenmaarten@gmail.com>
2019-08-08 11:32:45 +0200

    - Added function 'my_theme_register_footer_columns'.

--- inc/widgets.php
+++ inc/widgets.php
@@ -53,3 +53,36 @@ function my_theme_widgets_init() {
 }
 
 add_action( 'widgets_init', 'my_theme_widgets_init' );
+
+/**
+ * Register widget areas described as footer columns.
+ *
+ * @param int   $amount The amount of sidebars to register.
+ * @param array $args   Arguments for function register_sidebar.
+ */
+function my_theme_register_footer_columns( $amount, $args = array() ) {
+
+	$ordinals = array(
+		1 => __( 'first', 'my-theme' ),
+		2 => __( 'second', 'my-theme' ),
+		3 => __( 'thirth', 'my-theme' ),
+		4 => __( 'fourth', 'my-theme' ),
+		5 => __( 'fifth', 'my-theme' ),
+		6 => __( 'sixth', 'my-theme' ),
+		7 => __( 'seventh', 'my-theme' ),
+		8 => __( 'eighth', 'my-theme' ),
+		9 => __( 'ninth', 'my-theme' ),
+	);
+
+	for ( $n = 1; $n <= $amount; $n++ ) {
+		register_sidebar(
+			array(
+				'id'          => "footer-column-$n",
+				// translators: %d: column number.
+				'name'        => sprintf( esc_html__( 'Footer Column %d', 'my-theme' ), $n ),
+				// translators: %s: column number (ordinal).
+				'description' => sprintf( esc_html__( '%s column in footer section.', 'my-theme' ), ucfirst( $ordinals[ $n ] ) ),
+			) + $args
+		);
+	}
+}
