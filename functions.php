84d89c569c10fe2b6d1c67b8e8635307d86ddc09
Maarten Menten <mentenmaarten@gmail.com>
2019-08-08 21:17:04 +0200

    Simplified include.

--- functions.php
+++ functions.php
@@ -13,18 +13,18 @@ require get_template_directory() . '/inc/common.php';
 /**
  * Include files from inside the /inc directory.
  */
-$my_theme_includes = array(
-	'setup.php'              => true, // Set up theme defaults and register support for features.
-	'nav-menus.php'          => true, // Custom navigation menu functions.
-	'widgets.php'            => true, // Register widget areas.
-	'scripts.php'            => true, // Enqueue scripts and styles.
-	'template-tags.php'      => true, // Custom template tags for this theme.
-	'template-functions.php' => true, // Functions which enhance the theme by hooking into WordPress.
-	'editor.php'             => true, // Custom editor features.
-	'fields.php'             => true, // Custom fields related functions.
-	'options-page.php'       => true, // Add options page.
-	'blocks.php'             => true, // Register block types.
-	'hero-image.php'         => true, // Include hero image.
+my_theme_inc(
+	array(
+		'setup.php',              // Set up theme defaults and register support for features.
+		'nav-menus.php',          // Custom navigation menu functions.
+		'widgets.php',            // Register widget areas.
+		'scripts.php',            // Enqueue scripts and styles.
+		'template-tags.php',      // Custom template tags for this theme.
+		'template-functions.php', // Functions which enhance the theme by hooking into WordPress.
+		'editor.php',             // Custom editor features.
+		'fields.php',             // Custom fields related functions.
+		'options-page.php',       // Add options page.
+		'blocks.php',             // Register block types.
+		'hero-image.php',         // Include hero image.
+	)
 );
-
-my_theme_inc( array_keys( array_filter( $my_theme_includes ) ) );
