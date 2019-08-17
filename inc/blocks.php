4bcb598b2fceef5bd725b61bda56a0be4fe32e6b
Maarten Menten <mentenmaarten@gmail.com>
2019-08-18 01:06:25 +0200

    - added filter 'allowed_block_types' - added filter 'use_block_editor_for_post' - added function 'enqueue_block_assets'

--- inc/blocks.php
+++ inc/blocks.php
@@ -45,6 +45,77 @@ function register_block_types() {
 add_action( 'acf/init', __NAMESPACE__ . '\register_block_types' );
 
 /**
+ * Filter the allowed block types for the editor.
+ *
+// Set limitions.
+$allowed_block_types = array(
+	'core/block', // Required for reusable blocks.
+	'core/heading',
+	'core/paragraph',
+	'core/image',
+	'core/list',
+	'core/embed',
+	'core/html',
+	'core/shortcode',
+	'core/template',
+	'core/video',
+	'core/columns',
+);
+ *
+ * @uses acf_get_block_types()
+ *
+ * @param bool|array $allowed_block_types Array of block type slugs, or boolean to enable/disable all.
+ * @param object     $post                The post resource data.
+ *
+ * @return bool|array
+ */
+function allowed_block_types( $allowed_block_types, $post ) {
+
+	// Include our ACF block types when limitions are set.
+	if ( is_array( $allowed_block_types ) && function_exists( 'acf_get_block_types' ) ) {
+		$allowed_block_types = array_merge( $allowed_block_types, array_keys( acf_get_block_types() ) );
+	}
+
+	return $allowed_block_types;
+}
+
+add_filter( 'allowed_block_types', __NAMESPACE__ . 'allowed_block_types', 10, 2 );
+
+/**
+ * Filter whether a post is able to be edited in the block editor.
+ *
+ * @param bool    $use_block_editor Whether the post can be edited or not.
+ * @param WP_Post $post             The post being checked.
+ *
+ * @return bool
+ */
+function use_block_editor_for_post( $use_block_editor, $post ) {
+
+	return $use_block_editor;
+}
+
+add_filter( 'use_block_editor_for_post', __NAMESPACE__ . 'use_block_editor_for_post', 10, 2 );
+
+/**
+ * Enqueue block assets for both editor and front-end.
+ *
+// Dequeue WordPress front-end stylesheet
+wp_dequeue_style( 'wp-block-library' );
+ */
+function enqueue_block_assets() {
+
+	if ( ! is_admin() ) {
+		$theme         = wp_get_theme();
+		$theme_version = $theme->get( 'Version' );
+
+		$css_version = filemtime( get_template_directory() . '/dist/styles/blocks.css' );
+		wp_enqueue_style( 'my_theme-blocks', get_template_directory_uri() . '/dist/styles/blocks.css', array(), "{$theme_version}.{$css_version}" );
+	}
+}
+
+add_action( 'enqueue_block_assets', __NAMESPACE__ . '\enqueue_block_assets' );
+
+/**
  * Button block callback function
  *
  * @uses get_fields()
