8649866ac9523a9966b3b5c3f7953103c8ff31e7
Maarten Menten <mentenmaarten@gmail.com>
2019-08-20 00:51:52 +0200

    - Added custom image sizes. - Updated 'content_width'.

--- inc/setup.php
+++ inc/setup.php
@@ -85,6 +85,8 @@ function setup() {
 
 	// Add custom image sizes.
 	add_image_size( 'my-theme-full-width', 1920, 1080 );
+	add_image_size( 'my-theme-card', 540, 540 / 8 * 6, true );
+	add_image_size( 'my-theme-carousel', 1100, 1100 / 16 * 9, true );
 }
 
 add_action( 'after_setup_theme', __NAMESPACE__ . '\setup' );
@@ -100,7 +102,7 @@ function content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'my_theme_content_width', 1140 );
+	$GLOBALS['content_width'] = apply_filters( 'my_theme_content_width', 1100 );
 }
 
 add_action( 'after_setup_theme', __NAMESPACE__ . '\content_width', 0 );
