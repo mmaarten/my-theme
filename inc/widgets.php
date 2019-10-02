98ae3ab14fab5e2c63b4e6e9980d0cf0c1b22fea
Maarten Menten <mentenmaarten@gmail.com>
2019-10-02 16:40:08 +0200

    Cleanup

--- inc/widgets.php
+++ inc/widgets.php
@@ -19,7 +19,7 @@ function register_sidebars()
         'after_widget'  => '</section>',
         'before_title'  => '<h2 class="widget-title">',
         'after_title'   => '</h2>',
-    );
+    ];
 
     register_sidebar(
         [
