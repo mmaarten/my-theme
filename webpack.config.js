c1d43953582873d4050f4e0216489d7e49224087
Maarten Menten <mentenmaarten@gmail.com>
2019-10-02 15:38:44 +0200

    Added Babel.

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,16 @@ module.exports = {
   {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.(scss|sass|css)$/,
         use: [
           {
