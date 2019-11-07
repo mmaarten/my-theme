b12c717cf9a6b2f30afeebc1023366eb2354f6a0
Maarten Menten <mentenmaarten@gmail.com>
2019-11-07 21:20:11 +0100

    Cleanup

--- inc/helpers.php
+++ inc/helpers.php
@@ -7,6 +7,12 @@
 
 namespace My\Theme;
 
+/**
+ * Get the full asset URL with hashed filename.
+ *
+ * @param string $asset Relative path from build directory. e.g. 'scripts/main.js'.
+ * @return bool|string
+ */
 function asset_path($asset)
 {
     $file = get_template_directory() . '/build/assets.json';
