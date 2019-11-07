49908c4d6d3466ea11ef7f41cfbb488941214ef2
Maarten Menten <mentenmaarten@gmail.com>
2019-11-07 20:40:44 +0100

    Simplified code.

--- inc/helpers.php
+++ inc/helpers.php
@@ -9,19 +9,14 @@ namespace My\Theme;
 
 function asset_path($asset)
 {
-    static $paths = null;
-
-    if (is_null($paths)) {
-        $file = get_template_directory() . '/build/assets.json';
-        if (file_exists($file)) {
-            $paths = json_decode(file_get_contents($file), true);
-        } else {
-            $paths = [];
-        }
+    $file = get_template_directory() . '/build/assets.json';
+    if (!file_exists($file)) {
+        return false;
     }
 
-    if (is_array($paths) && isset($paths[$asset])) {
-        return get_template_directory_uri() . '/build/' . $paths[$asset];
+    $paths = json_decode(file_get_contents($file), true);
+    if (isset($paths[$asset])) {
+        return $paths[$asset];
     }
 
     return false;
