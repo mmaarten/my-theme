52b06110eaa13f2d1f32a325938acfc49552a5f4
Maarten Menten <mentenmaarten@gmail.com>
2019-11-23 01:00:34 +0100

    Added admin notices

--- functions.php
+++ functions.php
@@ -1,27 +1,27 @@
-<?php
-/**
- * Fire up the application.
- *
- * @package My/Theme
- */
+<?php // phpcs:disable PSR1.Files.SideEffects.FoundWithSymbols
 
-namespace My\Theme;
-
-$config = require get_parent_theme_path('config/core.php');
+$config = require get_parent_theme_file_path('config/core.php');
 
 /**
  * Check required PHP version.
  */
 if (version_compare(PHP_VERSION, $config['php_version'], '<')) {
-    error_log(
-        sprintf(
+
+    function my_theme_invalid_php_version_notice()
+    {
+        $config = $GLOBALS['config'];
+
+        $message = sprintf(
             // translators: 1: Theme name. 2: required PHP version. 3: active PHP version.
             __('%1$s requires at least PHP version %2$s. You are running version %3$s.', 'my-theme'),
             $config['theme_name'],
             $config['php_version'],
             PHP_VERSION
-        )
-    );
+        );
+
+        printf('<div class="notice notice-error"><p>%s</p></div>', esc_html($message));
+    }
+    add_action('admin_notices', 'my_theme_invalid_php_version_notice');
     return;
 }
 
@@ -29,15 +29,16 @@ if (version_compare(PHP_VERSION, $config['php_version'], '<')) {
  * Check required WordPress version.
  */
 if (version_compare($GLOBALS['wp_version'], $config['wp_version'], '<')) {
-    error_log(
-        sprintf(
+    add_action('admin_notices', function () {
+        $message = sprintf(
             // translators: 1: Theme name. 2: required WordPress version. 3: active WordPress version.
             __('%1$s requires at least WordPress version %2$s. You are running version %3$s.', 'my-theme'),
             $config['theme_name'],
             $config['wp_version'],
             $GLOBALS['wp_version']
-        )
-    );
+        );
+        printf('<div class="notice notice-error"><p>%s</p></div>', esc_html($message));
+    });
     return;
 }
 
@@ -76,9 +77,13 @@ array_map(function ($file) {
 /**
  * Load config.
  */
+
+use My\Theme\Container;
+use My\Theme\Config;
+
 Container::getInstance()->add('config', function () use ($config) {
-    $items = [];
-    foreach ((array)$config['autoload_config'] as $slug) {
+    $items = ['core' => $config];
+    foreach ($config['autoload_config'] as $slug) {
         $file = "config/{$slug}.php";
         if ($located = locate_template($file)) {
             $items[$slug] = require $located;
@@ -90,7 +95,5 @@ Container::getInstance()->add('config', function () use ($config) {
             );
         }
     }
-    $items['core'] = $config;
-
     return new Config($items);
 });
