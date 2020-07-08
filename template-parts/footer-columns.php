9e9c24fbeed04508d34c3d5156d7912feff6b194
Maarten Menten <mentenmaarten@gmail.com>
2020-07-08 13:35:52 +0200

    Added footer columns

---
+++ template-parts/footer-columns.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Footer columns
+ *
+ * @package My/Theme
+ */
+
+if (! array_filter(['footer-column-1', 'footer-column-2', 'footer-column-3'], 'is_active_sidebar')) {
+    return;
+}
+?>
+
+<aside id="footer-columns" class="widget-area" role="complementary">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md">
+
+                <?php dynamic_sidebar('footer-column-1'); ?>
+
+            </div>
+
+            <div class="col-md">
+
+                <?php dynamic_sidebar('footer-column-2'); ?>
+
+            </div>
+
+            <div class="col-md">
+
+                <?php dynamic_sidebar('footer-column-3'); ?>
+
+            </div>
+
+        </div><!-- .row -->
+
+    </div><!-- .container -->
+
+</aside><!-- .widget-area -->
