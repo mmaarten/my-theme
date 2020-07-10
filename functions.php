cb7b65f07d53284130cabe26d71bc2e8cafec0b9
Maarten Menten <mentenmaarten@gmail.com>
2020-07-10 20:33:19 +0200

    Cleanup

--- functions.php
+++ functions.php
@@ -85,6 +85,5 @@ use My\Theme\Config;
 Container::getInstance()->add('config', function () {
     return new Config([
         'assets' => require get_theme_file_path('config/assets.php'),
-        'icons'  => require get_theme_file_path('config/icons.php'),
     ]);
 });
