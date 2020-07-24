98ce9a3272a24285a694367b9e109916267960bf
Maarten Menten <mentenmaarten@gmail.com>
2020-07-24 20:42:12 +0200

    Cleanup

--- inc/helpers.php
+++ inc/helpers.php
@@ -8,37 +8,6 @@
 namespace My\Theme;
 
 /**
- * @param string $id
- * @param mixed $concrete
- * @param bool $shared
- * @return mixed
- */
-function app($key = null, $value = null, $shared = true)
-{
-    $container = Container::getInstance();
-
-    if (is_null($key)) {
-        return $container;
-    }
-
-    if (is_null($value)) {
-        return $container->get($key);
-    }
-
-    return $container->add($key, $value, $shared);
-}
-
-/**
- * @param string $key
- * @param mixed $default
- * @return mixed
- */
-function config($key, $default = null)
-{
-    return app('config')->get($key, $default);
-}
-
-/**
  * Register a series of sidebars described as footer columns.
  *
  * @param int   $amount The amount of sidebars to register.
