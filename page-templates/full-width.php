4e2faa50cebe2e6e2db343f176d122a7959ae4d7
Maarten Menten <>
2020-08-16 17:18:32 +0200

    Removed row and column

--- page-templates/full-width.php
+++ page-templates/full-width.php
@@ -14,41 +14,37 @@ get_header();
 
 <div class="container-fluid">
 
-    <div class="row">
+    <div id="primary" class="content-area">
 
-        <div id="primary" class="col-md content-area">
+        <main id="main" class="site-main">
 
-            <main id="main" class="site-main">
+        <?php
 
-            <?php
+        // Start the Loop.
+        while (have_posts()) :
+            // Setup postdata.
+            the_post();
 
-            // Start the Loop.
-            while (have_posts()) :
-                // Setup postdata.
-                the_post();
+            // Include the template for the content.
+            if ('page' === get_post_type()) {
+                get_template_part('template-parts/content', 'page');
+            } else {
+                get_template_part('template-parts/content', 'single');
+            }
 
-                // Include the template for the content.
-                if ('page' === get_post_type()) {
-                    get_template_part('template-parts/content', 'page');
-                } else {
-                    get_template_part('template-parts/content', 'single');
-                }
+            // If comments are open or we have at least one comment, load up the comment template.
+            if (comments_open() || get_comments_number()) :
+                comments_template();
+            endif;
 
-                // If comments are open or we have at least one comment, load up the comment template.
-                if (comments_open() || get_comments_number()) :
-                    comments_template();
-                endif;
+            // End of the Loop.
+        endwhile;
 
-                // End of the Loop.
-            endwhile;
+        ?>
 
-            ?>
+        </main><!-- #main -->
 
-            </main><!-- #main -->
-
-        </div><!-- #primary -->
-
-    </div><!-- .row -->
+    </div><!-- #primary -->
 
 </div><!-- .container -->
 
