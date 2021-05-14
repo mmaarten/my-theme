35354ed3f5657fdd598e2cc1489c3bdafb2d1e90
Maarten Menten <>
2021-05-14 14:24:13 +0200

    Added method 'getBlockHTMLAttributes'.

--- src/BlockTypes/AbstractDynamicBlock.php
+++ src/BlockTypes/AbstractDynamicBlock.php
@@ -10,6 +10,33 @@ namespace My\Theme\BlockTypes;
 abstract class AbstractDynamicBlock extends AbstractBlock
 {
     /**
+     * Get block HTML attributes.
+     *
+     * @param WP_Block $block
+     * @return array
+     */
+    protected static function getBlockHTMLAttributes($block)
+    {
+        $atts = [];
+
+        $atts['class'] = 'wp-block-' . str_replace('/', '-', $block->name);
+
+        if (! empty($block->attributes['className'])) {
+            $atts['class'] .= " {$block->attributes['className']}";
+        }
+
+        if (! empty($block->attributes['align'])) {
+            $atts['class'] .= " align{$block->attributes['align']}";
+        }
+
+        if (! empty($block->attributes['anchor'])) {
+            $atts['id'] = $block->attributes['anchor'];
+        }
+
+        return $atts;
+    }
+
+    /**
      * Get block type attributes
      *
      * @return array
