51800da9047060b21c2c921804f5af3904726984
Maarten Menten <>
2021-05-14 12:57:36 +0200

    Cleanup

---
+++ src/BlockTypes/Column.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Column block
+ *
+ * @package My/Theme/BlockTypes
+ */
+
+namespace My\Theme\BlockTypes;
+
+class Column extends AbstractBlock
+{
+    public function __construct()
+    {
+        parent::__construct('column');
+    }
+}
