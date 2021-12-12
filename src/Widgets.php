1a60477423ae3111286fc785968b770aa06c355f
Maarten Menten <>
2021-12-12 15:23:19 +0100

    Made code more compact.

--- src/Widgets.php
+++ src/Widgets.php
@@ -29,37 +29,29 @@ class Widgets
           'after_title'   => '</h2>',
         ];
 
-        register_sidebar(
-            [
-              'id'          => 'header',
-              'name'        => esc_html__('Header', 'my-theme'),
-              'description' => esc_html__('Header section.', 'my-theme'),
-            ] + $args
-        );
+        register_sidebar([
+            'id'          => 'header',
+            'name'        => esc_html__('Header', 'my-theme'),
+            'description' => esc_html__('Header section.', 'my-theme'),
+        ] + $args);
 
-        register_sidebar(
-            [
-              'id'          => 'sidebar-left',
-              'name'        => esc_html__('Left Sidebar', 'my-theme'),
-              'description' => esc_html__('Section on the left side.', 'my-theme'),
-            ] + $args
-        );
+        register_sidebar([
+            'id'          => 'sidebar-left',
+            'name'        => esc_html__('Left Sidebar', 'my-theme'),
+            'description' => esc_html__('Section on the left side.', 'my-theme'),
+        ] + $args);
 
-        register_sidebar(
-            [
-              'id'          => 'sidebar-right',
-              'name'        => esc_html__('Right Sidebar', 'my-theme'),
-              'description' => esc_html__('Section on the right side.', 'my-theme'),
-            ] + $args
-        );
+        register_sidebar([
+            'id'          => 'sidebar-right',
+            'name'        => esc_html__('Right Sidebar', 'my-theme'),
+            'description' => esc_html__('Section on the right side.', 'my-theme'),
+        ] + $args);
 
-        register_sidebar(
-            [
-              'id'          => 'footer',
-              'name'        => esc_html__('Footer', 'my-theme'),
-              'description' => esc_html__('Footer section.', 'my-theme'),
-            ] + $args
-        );
+        register_sidebar([
+            'id'          => 'footer',
+            'name'        => esc_html__('Footer', 'my-theme'),
+            'description' => esc_html__('Footer section.', 'my-theme'),
+        ] + $args);
 
         self::registerFooterColumns(3, $args);
     }
